fc85080a3d1500144c0e9001678f8508d3fd4b2d
Davneet Singh <davneet4u@gmail.com>
2015-01-05 00:11:47 +0530

    Added demo views to file

--- teach_project/categories/views.py
+++ teach_project/categories/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(Request):
+    '''This is a demo borrowed from tango with django
+       it has absolutely no use here'''
+    return HttpResponse("Davneet says I will fucking do it")
